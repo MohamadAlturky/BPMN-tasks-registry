a35c4c408fb549440f1070598788e07cad3bf056
mohamad.alturky <m799678@gmail.com>
2024-07-12 08:33:24 +0300

    adding extract-the-activities-from-the-text task

---
+++ src/crews/activities/extract-the-activities-from-the-text/main.py
@@ -0,0 +1,63 @@
+import os
+from langchain_community.llms import Ollama
+from crewai import Agent, Task, Crew
+from dotenv import load_dotenv
+from pydantic import BaseModel
+
+class Activity(BaseModel):
+    name: str
+    sequence: int
+
+class Activities(BaseModel):
+    activities: list[Activity]
+
+load_dotenv(override=True)
+
+llm = Ollama(model="llama3", base_url=os.getenv("OLLAMA_HOST"))
+
+agent = Agent(
+    role="business process analyst",
+    goal="Extract activities from the given process description '{process_description}' and organize them for BPMN diagram generation.",
+    backstory="As a business process analyst, you analyze process descriptions to extract and organize activities needed for creating BPMN diagrams.",
+    allow_delegation=False,
+    verbose=True,
+    llm=llm
+)
+
+task = Task(
+    description=(
+        "Extract the activities from the given process description '{process_description}'. "
+        "Each activity should represent a distinct step in the process. "
+        "List each activity in the order it occurs, ensuring each activity is clearly described and sequenced appropriately."
+    ),
+    expected_output=(
+        "A structured list of activities with their respective sequences, formatted for BPMN diagram creation."
+    ),
+    output_pydantic=Activities,
+    agent=agent,
+)
+
+executor = Crew(
+    agents=[agent],
+    tasks=[task],
+    verbose=2
+)
+
+inputs = {
+    "process_description": """
+        Consider a process for purchasing items from an online shop. The user starts an order by logging in to their account.
+        Then, the user simultaneously selects the items to purchase and sets a payment method. Afterward, the user either pays or completes an installment agreement.
+        Since the reward value depends on the purchase value, after selecting the items, the user chooses between multiple options for a free reward.
+        This step is done after selecting the items, but it is independent of the payment activities.
+        Finally, the items are delivered. The user has the right to return items for exchange. Every time items are returned, a new delivery is made.
+    """
+}
+
+result: Activities = executor.kickoff(inputs)
+
+for i in result.activities:
+    print("name")
+    print(i.name)
+    print("sequence")
+    print(i.sequence)
+    print()
